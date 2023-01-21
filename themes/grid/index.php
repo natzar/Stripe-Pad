9bc1a666efb57dfbc9ee8d15f7b3e9b86d15ddd0
Beto <betolopezayesa@gmail.com>
2023-01-21 02:02:29 +0100

    Replace hardcoded values in html by values from stripe json

--- themes/grid/index.php
+++ themes/grid/index.php
@@ -24,9 +24,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-	<title>Stripe Pad</title>
-	  <meta name="title" content="Stripe Pad">
-	  <meta name="author" content="Php Ninja">
+	<title><?= $settings['account']['settings']['dashboard']['display_name'] ?></title>
+	  <meta name="title" content="Stripe Pad">	  
 	  <meta name="description" content="Stripe Pad, create a public shop from your Stripe products in a minute">
 	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	  <meta charset='utf-8'>   
@@ -35,19 +34,16 @@
   </head>
   <body>
 
-   
-
-
 	<div class="bg-white">
   <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 	<span class="bg-indigo-100 rounded-full text-indigo-600 px-4 py-1 text-xs uppercase font-extrabold">Just launched!</span>
-   <h2 class="text-6xl font-extrabold tracking-tight text-gray-900 mt-5">Store Front for Stripe</h2>
-   <p class="text-2xl font-light text-gray-500 mb-10">Create a public shop with your stripe products, it's free<br>stripepad.com/your-username</p>
+   <h2 class="text-6xl font-extrabold tracking-tight text-gray-900 mt-5"><?= $settings['account']['settings']['dashboard']['display_name'] ?></h2>
+   <p class="text-2xl font-light text-gray-500 mb-10">Edit /themes/<?= Theme ?>/index.php file to change this text</p>
    
    
-<a class="rounded-full bg-indigo-600 text-white py-3 px-5 inline" href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_M1NA1F5GhfLiTMO1wbV65OYRJ9COTea8&scope=read_write">Sign in with Stripe</a>
+<a class="rounded-full bg-indigo-600 text-white py-3 px-5 inline" href="javascript:alert('Maybe copy link from a product and use it on this button');">Sign up</a>
 
-<p class="mt-10 text-gray-500 inline">Sign in to check your store, no commitment. </p>
+<p class="mt-10 text-gray-500 inline">Probably you have more than one product, not sure which one to pick for Sign Up butotn</p>
   
 
 <div class="bg-white">
@@ -55,102 +51,8 @@
 	<h2 class="text-2xl font-extrabold">Your Shop</h2>
 
 	<div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-	  <a href="#" class="group">
-		<div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-		  <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
-		</div>
-		<h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-		<p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-	  </a>
-
-	  <a href="#" class="group">
-		<div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-		  <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="w-full h-full object-center object-cover group-hover:opacity-75">
-		</div>
-		<h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-		<p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-	  </a>
-
-	  <a href="#" class="group">
-		<div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-		  <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="w-full h-full object-center object-cover group-hover:opacity-75">
-		</div>
-		<h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-		<p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-	  </a>
-
-	  <a href="#" class="group">
-		<div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-		  <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75">
-		</div>
-		<h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-		<p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-	  </a>
-
-	  <!-- More products... -->
-	</div>
-  </div>
-</div>
-
-
-  
-  
-	<div class="border-t-1 border-t border-indigo-200 mt-10 text-gray-400 pt-3 text-xs mt-10 block"><strong>Stripepad.com</strong> is not associated with Stripe. &copy; & Feedback <a  rel="nofollow" class="underline text-indigo-600" href="https://www.twitter.com/betoayesa" target="_blank">Twitter</a></div>
-  </div>
-  
-	</div>
-	
-
-<template>
-  <h2>Flower</h2>
-  <img src="img_white_flower.jpg" width="214" height="204">
-</template>
-
-<template id="grid">
-
-
-
-
-
-  <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-10">
-  
-   
 	  
-
-
-
-<? foreach ($products as $p): ?>
-<? if (empty($p->images)) $p->images[] = "/product-placeholder.png"; ?>
-  
-	<div href="#" class="group overflow-hidden mb-10">
-		<div class="  bg-gray-100  overflow-hidden ">
-		  <img height="200" src="<?= $p->images[0] ?>" alt="<?= $p->name ?>" class="rounded-full aspect-w-1 aspect-h-1 object-center object-cover group-hover:opacity-75">
-		</div>
-		<h3 class="mt-4 text-sm font-semibold text-gray-700"><?= $p->name ?></h3>
-		<p class="mt-1 text-xs font-light text-gray-400"><?= $p->description ?></p>
-		<p class="mt-1 text-lg font-medium text-gray-900"></p>
-		<? foreach($p->price as $p): ?>
-		<a href="<?= $p->link->url ?>" class="py-1 rounded-lg px-2 bg-indigo-300 text-white hover:bg-indigo-600">$<?= $p->amount / 100 ?></a>
-		
-		<? endforeach; ?>
-	  </div>
-
-
-<? endforeach; ?>
-	  <!-- More products... -->
-	</div>
-
-
-</template>
-
-<template id="grid2">
-
-   <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-	  
-
-
-<pre><? print_r($products->data[5]); ?></pre>
-<? foreach ($products->autoPagingIterator() as $p): ?>
+		<? foreach ($settings['products'] as $p): ?>
 <? if (!$p->images[0]) $p->images[0] = "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"; ?>
   
 	<div class="group relative ">
@@ -178,12 +80,23 @@
 		</div>
 	  </div>
 <? endforeach; ?>
-	  <!-- More products... -->
+
 	</div>
-  </template>
-  
-	<div class="border-t-1 border-t border-indigo-200 mt-10 text-gray-400 pt-3 text-xs mt-10 block"><?= $shop['title'] ?> @ Powered by <a class="hover:text-indigo-600" href="https://www.stripead.com">Stripepad.com</a> <br> 
+  </div>
 </div>
+
+
+  
+  
+	<div class="border-t-1 border-t border-indigo-200 mt-10 text-gray-400 pt-3 text-xs mt-10 block">
+	 &copy; <?= $settings['account']['settings']['dashboard']['display_name'] ?> is powered by	<strong>Stripepad.com</strong>. Send feedback <a  rel="nofollow" class="underline text-indigo-600" href="mailto:<?= AdminEmail ?>" target="_blank">Email</a></div>
+  </div>
+  
+	</div>
+	
+
+
+
 
   </body>
 </html>
