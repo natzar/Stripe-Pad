48ceec38158f1eded7779e1e05a66542b13742e5
Beto <betolopezayesa@gmail.com>
2023-01-21 20:54:41 +0100

    Old whatsapp 'plugin'

--- plugins/whatsapp/whatsapp.php
+++
@@ -1,7 +0,0 @@
-<div id="whatsapp" style="display: block !important;display: block;
-    position: fixed;
-    bottom: 10px;
-    right: 10px;
-    z-index: 999;
-    width: 80px;"> <a  href="whatsapp://send?phone=+34649382965"> <img alt="icono whatsapp web developer" src="/whatsapp-programador-web.png"></a></div>
-
