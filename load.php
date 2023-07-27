989bc4c163bdb0ca6af0d6a1def1f5742e99f8c9
Beto <betolopezayesa@gmail.com>
2023-07-27 21:34:56 +0200

    Reset

--- load.php
+++ load.php
@@ -4,6 +4,7 @@
 	Stripe Pad Load
                                                                                                                                                  
 */    
+
 if (PHP_SESSION_ACTIVE != session_status() and !headers_sent()){
 	ini_set('session.cookie_lifetime', 3600 * 24 );
 	ini_set('session.gc_maxlifetime', 3600 * 24 );
@@ -18,25 +19,29 @@ date_default_timezone_set('Europe/Madrid');
 setlocale (LC_ALL, 'es_ES.ISO8859-1'); 
 setlocale(LC_TIME, 'spanish'); 
 
+require_once dirname(__FILE__).'/config.php';
 
+if (is_file(dirname(__FILE__)."/vendor/autoload.php")) require(CORE_PATH."vendor/autoload.php");
 
-if (!defined("CORE_PATH")) define('CORE_PATH',dirname(__FILE__)."/");
+include_once CORE_PATH."ControllerBase.php";
+include_once CORE_PATH."ModelBase.php";
+require_once CORE_PATH.'Config.php'; //de configuracion
+require_once CORE_PATH.'SPDO.php';
+require CORE_PATH.'View.php';
 
-require(CORE_PATH."vendor/autoload.php");
+if (DEBUG_MODE){
+	set_error_handler(function($errno, $errstr, $errfile, $errline ){
+		
+		$error_msg = Date("d/m/Y H:i:s")." ".$errstr." [". $errno."]"." File: ". $errfile. " // Linea: ".$errline." ";		
+		file_put_contents(APP_PATH.APP_NAME."-errors.log", $error_msg);
+		echo $error_msg. '<br>';
+	   // throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+	});
+}
 
+include_once CORE_PATH."functions.php";	
 
-
-include_once CORE_PATH."lib/ControllerBase.php";
-include_once CORE_PATH."lib/ModelBase.php";
-require_once CORE_PATH.'lib/Config.php'; //de configuracion
-require_once CORE_PATH.'lib/SPDO.php';
-
-require_once CORE_PATH.'config.php';
-
-
-include_once CORE_PATH."lib/functions.php";	
-
-foreach (glob(dirname(__FILE__)."/app/models*.php") as $filename)
+foreach (glob(dirname(__FILE__)."/app/models/*.php") as $filename)
 {
     include $filename;
 }
