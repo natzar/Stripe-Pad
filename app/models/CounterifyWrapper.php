b00f5821d50cc37485f4f50037d6195166041bdc
Beto <betolopezayesa@gmail.com>
2023-10-29 12:40:46 +0100

    no message

--- app/models/CounterifyWrapper.php
+++ app/models/CounterifyWrapper.php
@@ -3,38 +3,53 @@
  * Package Name: Stripe Pad <> Counterify
  * File Description: This file handles connection with counterify.com server. Counterify is a service to track and count everything from anything.
  * 
- * @author John Doe <john.doe@example.com>
+ * @author Beto Ayesa <beto.phpninja@gmail.com>
  * @version 1.0.0
- * @package Counterify PHP SDK
+ * @package StripePad
  * @license GPL3
- * @link https://github.com/johndoe/my-awesome-php-package
+ * @link https://github.com/natzar/stripe-pad
  * 
  */
 
 class Counterify {
 
-		public function push( $usersId, $kpi, $value){
+	private static $instance = null;
 
-			$consulta = $this->db->prepare("
-				INSERT INTO datatracker (hash,week,month,customersId,usersId,kpi,counter)
-				VALUES  (:hash,YEARWEEK(CURDATE()),extract(YEAR_MONTH FROM CURDATE()), :customersId,:usersId, :kpi, :counter)");
-			
-			$hash = $this->fingerprint($kpi);
-			$consulta->bindParam(":hash",$hash);
-			$consulta->bindParam(":customersId",$_SESSION['user']['customersId']);
-			$consulta->bindParam(":usersId",$_SESSION['user']['usersId']);
-			$consulta->bindParam(":kpi",$kpi);
-			$consulta->bindParam(":counter",$value);
-			$consulta->execute();
-			
-			if ($consulta->rowCount() > 0){	
-				$q = $this->db->prepare("SELECT * from datatracker where id = LAST_INSERT_ID()");
-				$q->execute();
-				return $q->fetch();
-			}else {
-				return array();
-			}
+	public static function singleton()
+	{
+		if( self::$instance == null )
+		{
+			self::$instance = new self();
 		}
+		return self::$instance;
+	}
+
+	public function push($key,$value = 1){
+
+		$url = 'https://api.counterify.com/counter';
+
+		$params = array(
+		  'label' => $key,
+		  'count' => $value
+		);
+
+		$headers = array(
+		  'Authorization: Bearer ' . COUNTERIFY_TOKEN,
+		  'Content-Type: application/json'
+		);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1000);
+		curl_exec($ch);
+		curl_close($ch);
+
+
+	}
 		
 	
 }
