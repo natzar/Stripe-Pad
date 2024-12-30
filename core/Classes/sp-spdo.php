cabecda63f02f464007e8672c8e5f5a9dd8c2a79
Beto <beto@ayesadigital.com>
2024-12-30 09:36:08 +0100

    fix SPDO bug

--- core/Classes/sp-spdo.php
+++ core/Classes/sp-spdo.php
@@ -13,6 +13,7 @@ class SPDO extends PDO
 			parent::__construct('mysql:host=' . APP_DB_HOST . ';dbname=' . APP_DB, APP_DB_USER, APP_DB_PASSWORD);
 			parent::exec("SET NAMES utf8mb4;"); //$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 			$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 			$this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		} catch (PDOException $e) {
 
