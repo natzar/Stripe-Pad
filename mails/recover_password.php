1738e7c3de070a7f0f9fef28e2eb96645caa9bee
Beto <beto@ayesadigital.com>
2025-01-01 14:07:37 +0100

    Fix mail templates lang strings

--- mails/recover_password.php
+++ mails/recover_password.php
@@ -1,5 +1,5 @@
-<p>Se ha generado un nuevo password para tu cuenta:</p>
-<p><strong>Usuario:</strong> {{user}}<br />
+<p>New password generated:</p>
+<p><strong>User:</strong> {{user}}<br />
     <strong>Password:</strong> {{password}} <br /><br />
 
 </p>
@@ -11,7 +11,7 @@
                 <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                     <tbody>
                         <tr>
-                            <td> <a href="<?= APP_DOMAIN ?>" target="_blank">Entrar ahora</a> </td>
+                            <td> <a href="<?= APP_DOMAIN ?>login" target="_blank">Login now</a> </td>
                         </tr>
                     </tbody>
                 </table>
@@ -19,5 +19,4 @@
         </tr>
     </tbody>
 </table>
-<p>Si no has sido tu qui&eacute;n ha pedido un cambio de password para acceder al &aacute;rea de clientes de Php Ninja,
-    por favor escr&iacute;benos a hola@phpninja.es.</p>
+<p>If you haven't requested a password reset contact <?= ADMIN_EMAIL ?>.</p>
--- mails/welcome_user.php
+++ mails/welcome_user.php
@@ -1,6 +1,6 @@
-<p>Bienvenid@ {{name}},</p>
+<p>Welcome {{name}},</p>
 
-<p>Es un placer darte la bienvenida a Php Ninja, ya dispones de acceso al área de clientes:
+<p>
     <br><br>
     <strong>Usuario:</strong> {{email}}<br>
 
@@ -15,7 +15,7 @@
                 <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                     <tbody>
                         <tr>
-                            <td align="center"> <a href="https://app.phpninja.net/login" target="_blank">Entrar ahora</a> </td>
+                            <td align="center"> <a href="<?= APP_DOMAIN ?>login" target="_blank">Login now</a> </td>
                         </tr>
                     </tbody>
                 </table>
@@ -24,26 +24,3 @@
     </tbody>
 </table>
 <br>
-<p><strong>Desde el área de clientes puedes:</strong></p>
-<ul>
-    <li>Añadir sitios web para monitorizar y activar el seguimiento (pagespeed, changelog, downtime ...)</li>
-    <li>Añadir credenciales y accesos de forma segura</li>
-    <li>Encargar tareas y abrir nuevos proyectos</li>
-    <li>Gestionar la tareas abiertas, en progreso y completadas</li>
-    <li>Descargar facturas</li>
-    <li>Obtener información, estadísticas y sugerencias de mejora de tu sitio web</li>
-</ul>
-
-
-<br>
-<p>👉 <strong>Próximo paso: Credenciales de tu sitio web</strong></p>
-<p>Añade los credenciales de forma segura, desde el área de clientes, para que podamos realizar cambios. Sobretodo necesitamos acceso a hosting, al FTP y al administrador.</p>
-<br>
-<p><strong>Contratar Servicios</strong></p>
-<p>Contrata nuestros servicios directamente desde el área de clientes, o pide información sobre cualquier servicio respondiendo a este mismo email.</p>
-<br>
-<p><strong>Acuerdo de confidencialidad</strong></p>
-<p>Rellena tus datos desde el área de clientes y descarga nuestro acuerdo de confidencialidad básico.</p>
-<br>
-<p><strong>EMERGENCIAS</strong></p>
-<p>Whatsapp +34 649 38 29 65</p>
