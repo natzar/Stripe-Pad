aeefe991441f5e4deab84cc55f3debf1cf634743
Beto <beto@ayesadigital.com>
2025-01-01 14:21:36 +0100

    Fix form

--- app/views/superadmin/form.php
+++ app/views/superadmin/form.php
@@ -19,47 +19,47 @@
 		<main class="flex-1 text-gray-800">
 			<div class=" main max-w-2xl mx-auto bg-white rounded-lg px-6 py-5 mb-10">
 				<!-- Notification -->
-
-				<nav class="flex items-center justify-between space-x-2" aria-label="Breadcrumb">
-					<a href="<?= APP_DOMAIN ?>table/<?= $table ?>" class="flex justify-start text-gray-500 hover:font-bold hover:text-gray-500 hover:bg-gray-100 rounded-full px-3">
-						<ol role="list" class="flex space-x-3 ">
-							<li class="flex">
-								<div class="flex items-center">
-
-									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-										<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-									</svg>
-
-
-								</div>
-							</li>
-							<li class="flex">
-								<div class="flex text-lg items-center">
-
-									Back
-								</div>
-							</li>
-
-
-						</ol>
-					</a>
-					<button class='flex items-center justify-end px-4 py-1 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-orange-500 hover:bg-orange-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900' onclick="check_form_values(this.form);" type="button"><i class="glyphicon glyphicon-ok"></i> Save  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-							<path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-						</svg>
-					</button>
-				</nav>
-
-				<? if (isset($_GET['i']) and $_GET['i'] == 'success'): ?>
-					<div class="alert alert-success">
-						<a class="close" data-dismiss='alert'>&times;</a>
-						<strong>OK</strong> <?= $notification ?>
-					</div>
-				<? endif; ?>
-
-
-
-
 				<form class='form  py-3' id="form<?= $table ?><?= $rid ?>" name="<?= APP_BASE_URL ?>form<?= $table ?><?= $rid ?>" action="update" method="POST" enctype="multipart/form-data">
+
+					<nav class="flex items-center justify-between space-x-2" aria-label="Breadcrumb">
+						<a href="<?= APP_DOMAIN ?>table/<?= $table ?>" class="flex justify-start text-gray-500 hover:font-bold hover:text-gray-500 hover:bg-gray-100 rounded-full px-3">
+							<ol role="list" class="flex space-x-3 ">
+								<li class="flex">
+									<div class="flex items-center">
+
+										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+											<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+										</svg>
+
+
+									</div>
+								</li>
+								<li class="flex">
+									<div class="flex text-lg items-center">
+
+										Back
+									</div>
+								</li>
+
+
+							</ol>
+						</a>
+						<button class='flex items-center justify-end px-4 py-1 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-orange-500 hover:bg-orange-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900' onclick="check_form_values(this.form);" type="button"><i class="glyphicon glyphicon-ok"></i> Save <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+							</svg>
+						</button>
+					</nav>
+
+					<? if (isset($_GET['i']) and $_GET['i'] == 'success'): ?>
+						<div class="alert alert-success">
+							<a class="close" data-dismiss='alert'>&times;</a>
+							<strong>OK</strong> <?= $notification ?>
+						</div>
+					<? endif; ?>
+
+
+
+
 
 
 					<fieldset>
