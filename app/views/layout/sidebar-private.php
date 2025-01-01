742d284a4e64e850dbfb2cd7d3e0c98a82a1a66b
Beto <beto@ayesadigital.com>
2025-01-01 14:23:33 +0100

    fix sidebar private 1/2

--- app/views/layout/sidebar-private.php
+++ app/views/layout/sidebar-private.php
@@ -15,7 +15,7 @@
             <? if ($_SESSION['user']['group'] == 'superadmin') : ?>
                 <li>
                     <!-- Current: "bg-gray-50 text-indigo-600", Default: "text-gray-400 hover:text-indigo-600 hover:bg-gray-50" -->
-                    <a href="<?= APP_DOMAIN ?>superadmin" class="group flex gap-x-3 rounded-md bg-sky-700 p-2 text-sm/6 font-semibold text-gray-100">
+                    <a href="<?= APP_DOMAIN ?>superadmin" class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-400 hover:bg-gray-50 hover:text-indigo-600">
                         <svg class="size-6 shrink-0 text-gray-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
