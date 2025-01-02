d3df615600528827ec80fe215419f06b2d07b38d
Beto <beto@ayesadigital.com>
2025-01-02 12:59:08 +0100

    add gif

--- app/views/common/coming-soon.php
+++ app/views/common/coming-soon.php
@@ -2,9 +2,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
     <style>
         .handwritten {
-            font-family: 'Dancing Script', cursive;
+            font-family: 'Dancing Script', cursive !important;
         }
         </style>
         <div class="min-h-screen">
     <h1 class="mx-auto text-6xl text-gray-800 handwritten text-center font-bold">Coming Soon!</h1>
+
+
+    <iframe src="https://giphy.com/embed/26BRuo6sLetdllPAQ" width="480" height="336" style="" frameBorder="0" class="giphy-embed mx-auto mt-5" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/siliconvalleyhbo-watching-goodbye-window-26BRuo6sLetdllPAQ">via GIPHY</a></p>
     </div>
