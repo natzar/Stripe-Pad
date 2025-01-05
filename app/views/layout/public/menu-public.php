1b629a622de2ed339f51279591799d57078f476c
Beto <beto@ayesadigital.com>
2025-01-05 15:33:01 +0100

    APP LOGO

--- app/views/layout/public/menu-public.php
+++ app/views/layout/public/menu-public.php
@@ -20,10 +20,8 @@
       <div class="flex lg:flex-1">
         <a href="<?= HOMEPAGE_URL ?>" class="-m-1.5 p-1.5 text-gray-100 hover:text-sky-500 font-bold">
           <span class="sr-only"><?= APP_NAME ?></span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
-          </svg>
-          <span class="nunito"><?= APP_NAME ?></span>
+          <img src="<?= APP_LOGO ?>" width="40" class="inline">
+          <span class=""><?= APP_NAME ?></span>
         </a>
       </div>
       <div class="flex lg:hidden">
@@ -64,12 +62,7 @@
                 <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
               </svg></div><span class="truncate text-gray-300">Star us on GitHub</span>
           </a>
-          <a href="https://stripepad.com/#download">
-            <button class="hidden rounded bg-sky-500 px-2.5 py-1 text-xs text-white transition duration-200 ease-out hover:bg-sky-400 lg:block">
-              Download Stripe Pad
 
-            </button>
-          </a>
           <a href="https://github.com/natzar/Stripe-Pad" target="_blank" rel="noreferrer" class="hidden border-b-2 border-transparent py-5 text-neutral-700 hover:border-sky-500 hover:text-sky-500 hover:opacity-75 lg:flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <title>GitHub</title>
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
