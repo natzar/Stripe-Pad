a9170254bea91da7f767c380f190986e8cbcbedf
Beto <beto@ayesadigital.com>
2025-01-06 22:35:00 +0100

    Refix

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -3,8 +3,8 @@
 
 <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-md">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-    <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+    <img class="mx-auto h-10 w-auto" src="<?= APP_LOGO ?>" alt="<?= APP_NAME ?>">
+    <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-100">Sign in to your account</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
