1887d5fb3282707c173511bab5b4c4813ec17075
Beto <beto@ayesadigital.com>
2025-01-06 22:26:51 +0100

    Base for Exceptions

--- core/Exceptions/sp-exceptions.php
+++ core/Exceptions/sp-exceptions.php
@@ -1,33 +1,23 @@
 <?php
 // Base custom exception which other exceptions can extend
-class BaseCustomException extends Exception
+namespace StripePad\Exceptions;
+
+use Exception;
+
+
+class StripePadException extends Exception
 {
+
     public function __construct($message, $code = 0, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
-    }
-
-    public function customLog()
-    {
-        // Log the error details somewhere
-        error_log($this->message);
+        $_SESSION['errors'][] = $message;
     }
 }
 
-// Specific custom exception for database errors
-class DatabaseException extends BaseCustomException
-{
-    public function additionalInfo()
-    {
-        return "Database connection failed: " . $this->getMessage();
-    }
-}
+class PermissionsException extends StripePadException {}
+class DatabaseException extends StripePadException {}
 
-// Specific custom exception for user input errors
-class InputException extends BaseCustomException
-{
-    public function suggestSolution()
-    {
-        return "Please check the provided input values.";
-    }
-}
+class FileSystemException extends StripePadException {}
+
+class ValidationException extends StripePadException {}
