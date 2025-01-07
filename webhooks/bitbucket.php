5fd018c43d736e4370b69a3fb3833d1ac7ae7fa2
Beto <beto@ayesadigital.com>
2025-01-07 21:00:35 +0100

    Invoices => Payments

--- webhooks/bitbucket.php
+++ webhooks/bitbucket.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Simple PHP Git deploy script
  *
@@ -26,7 +27,7 @@ if (!defined('SECRET_ACCESS_TOKEN')) define('SECRET_ACCESS_TOKEN', 'BetterChange
  *
  * @var string
  */
-if (!defined('REMOTE_REPOSITORY')) define('REMOTE_REPOSITORY', 'git@bitbucket.org:gophpninja/chatleads.git');
+if (!defined('REMOTE_REPOSITORY')) define('REMOTE_REPOSITORY', 'git@bitbucket.org:[insert repo]');
 
 /**
  * The branch that's being deployed.
@@ -42,7 +43,7 @@ if (!defined('BRANCH')) define('BRANCH', 'master');
  *
  * @var string Full path including the trailing slash
  */
-if (!defined('TARGET_DIR')) define('TARGET_DIR', dirname(__FILE__).'/../');
+if (!defined('TARGET_DIR')) define('TARGET_DIR', dirname(__FILE__) . '/../');
 
 /**
  * Whether to delete the files that are not in the repository but are on the
@@ -66,7 +67,9 @@ if (!defined('DELETE_FILES')) define('DELETE_FILES', false);
  * @var serialized array of strings
  */
 if (!defined('EXCLUDE')) define('EXCLUDE', serialize(array(
-	'.git','app/config.php','vendor'
+	'.git',
+	'app/config.php',
+	'vendor'
 )));
 
 /**
@@ -77,7 +80,7 @@ if (!defined('EXCLUDE')) define('EXCLUDE', serialize(array(
  *
  * @var string Full path including the trailing slash
  */
-if (!defined('TMP_DIR')) define('TMP_DIR', '/tmp/spgd-'.md5(REMOTE_REPOSITORY).'/');
+if (!defined('TMP_DIR')) define('TMP_DIR', '/tmp/spgd-' . md5(REMOTE_REPOSITORY) . '/');
 
 /**
  * Whether to remove the TMP_DIR after the deployment.
@@ -91,7 +94,7 @@ if (!defined('CLEAN_UP')) define('CLEAN_UP', false);
  *
  * @var string Full path to the file name
  */
-if (!defined('VERSION_FILE')) define('VERSION_FILE', TMP_DIR.'VERSION');
+if (!defined('VERSION_FILE')) define('VERSION_FILE', TMP_DIR . 'VERSION');
 
 /**
  * Time limit for each command.
@@ -136,7 +139,7 @@ if (!defined('COMPOSER_OPTIONS')) define('COMPOSER_OPTIONS', '--no-dev');
  * @var string Path to the COMPOSER_HOME e.g. `/tmp/composer`
  * @link https://getcomposer.org/doc/03-cli.md#composer-home
  */
-if (!defined('COMPOSER_HOME')) define('COMPOSER_HOME','/usr/home/domstl/tmp/composer');
+if (!defined('COMPOSER_HOME')) define('COMPOSER_HOME', '/usr/home/domstl/tmp/composer');
 
 
 /**
@@ -151,28 +154,47 @@ if (!defined('EMAIL_ON_ERROR')) define('EMAIL_ON_ERROR', 'beto.phpninja@gmail.co
 // ===========================================[ Configuration end ]===
 
 // If there's authorization error, set the correct HTTP header.
-if (!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN ) {
+if (!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN) {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true, 403);
 }
 ob_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="robots" content="noindex">
 	<title>Simple PHP Git deploy script</title>
 	<style>
-body { padding: 0 1em; background: #222; color: #fff; }
-h2, .error { color: #c33; }
-.prompt { color: #6be234; }
-.command { color: #729fcf; }
-.output { color: #999; }
+		body {
+			padding: 0 1em;
+			background: #222;
+			color: #fff;
+		}
+
+		h2,
+		.error {
+			color: #c33;
+		}
+
+		.prompt {
+			color: #6be234;
+		}
+
+		.command {
+			color: #729fcf;
+		}
+
+		.output {
+			color: #999;
+		}
 	</style>
 </head>
+
 <body>
 
-<pre>
+	<pre>
 
 Checking the environment ...
 
@@ -192,15 +214,16 @@ if (defined('USE_COMPOSER') && USE_COMPOSER === true) {
 	$requiredBinaries[] = 'composer --no-ansi';
 }
 foreach ($requiredBinaries as $command) {
-	$path = trim(shell_exec('which '.$command));
+	$path = trim(shell_exec('which ' . $command));
 	if ($path == '') {
 		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 		die(sprintf('<div class="error"><b>%s</b> not available. It needs to be installed on the server for this script to work.</div>', $command));
 	} else {
-		$version = explode("\n", shell_exec($command.' --version'));
-		printf('<b>%s</b> : %s'."\n"
-			, $path
-			, $version[0]
+		$version = explode("\n", shell_exec($command . ' --version'));
+		printf(
+			'<b>%s</b> : %s' . "\n",
+			$path,
+			$version[0]
 		);
 	}
 }
@@ -208,7 +231,7 @@ foreach ($requiredBinaries as $command) {
 
 Environment OK.
 
-Deploying <?php echo REMOTE_REPOSITORY; ?> <?php echo BRANCH."\n"; ?>
+Deploying <?php echo REMOTE_REPOSITORY; ?> <?php echo BRANCH . "\n"; ?>
 to        <?php echo TARGET_DIR; ?> ...
 
 <?php
@@ -220,24 +243,24 @@ $commands = array();
 if (!is_dir(TMP_DIR)) {
 	// Clone the repository into the TMP_DIR
 	$commands[] = sprintf(
-		'git clone --depth=1 --branch %s %s %s'
-		, BRANCH
-		, REMOTE_REPOSITORY
-		, TMP_DIR
+		'git clone --depth=1 --branch %s %s %s',
+		BRANCH,
+		REMOTE_REPOSITORY,
+		TMP_DIR
 	);
 } else {
 	// TMP_DIR exists and hopefully already contains the correct remote origin
 	// so we'll fetch the changes and reset the contents.
 	$commands[] = sprintf(
-		'git --git-dir="%s.git" --work-tree="%s" fetch --tags origin %s'
-		, TMP_DIR
-		, TMP_DIR
-		, BRANCH
+		'git --git-dir="%s.git" --work-tree="%s" fetch --tags origin %s',
+		TMP_DIR,
+		TMP_DIR,
+		BRANCH
 	);
 	$commands[] = sprintf(
-		'git --git-dir="%s.git" --work-tree="%s" reset --hard FETCH_HEAD'
-		, TMP_DIR
-		, TMP_DIR
+		'git --git-dir="%s.git" --work-tree="%s" reset --hard FETCH_HEAD',
+		TMP_DIR,
+		TMP_DIR
 	);
 }
 
@@ -249,10 +272,10 @@ $commands[] = sprintf(
 // Describe the deployed version
 if (defined('VERSION_FILE') && VERSION_FILE !== '') {
 	$commands[] = sprintf(
-		'git --git-dir="%s.git" --work-tree="%s" describe --always > %s'
-		, TMP_DIR
-		, TMP_DIR
-		, VERSION_FILE
+		'git --git-dir="%s.git" --work-tree="%s" describe --always > %s',
+		TMP_DIR,
+		TMP_DIR,
+		VERSION_FILE
 	);
 }
 
@@ -260,25 +283,25 @@ if (defined('VERSION_FILE') && VERSION_FILE !== '') {
 // without the BACKUP_DIR for the case when it's inside the TARGET_DIR
 if (defined('BACKUP_DIR') && BACKUP_DIR !== false) {
 	$commands[] = sprintf(
-		"tar --exclude='%s*' -czf %s/%s-%s-%s.tar.gz %s*"
-		, BACKUP_DIR
-		, BACKUP_DIR
-		, basename(TARGET_DIR)
-		, md5(TARGET_DIR)
-		, date('YmdHis')
-		, TARGET_DIR // We're backing up this directory into BACKUP_DIR
+		"tar --exclude='%s*' -czf %s/%s-%s-%s.tar.gz %s*",
+		BACKUP_DIR,
+		BACKUP_DIR,
+		basename(TARGET_DIR),
+		md5(TARGET_DIR),
+		date('YmdHis'),
+		TARGET_DIR // We're backing up this directory into BACKUP_DIR
 	);
 }
 
 // Invoke composer
 if (defined('USE_COMPOSER') && USE_COMPOSER === true) {
 	$commands[] = sprintf(
-		'/usr/bin/composer --no-ansi --no-interaction --no-progress --working-dir=%s install %s'
-		, TMP_DIR
-		, (defined('COMPOSER_OPTIONS')) ? COMPOSER_OPTIONS : ''
+		'/usr/bin/composer --no-ansi --no-interaction --no-progress --working-dir=%s install %s',
+		TMP_DIR,
+		(defined('COMPOSER_OPTIONS')) ? COMPOSER_OPTIONS : ''
 	);
 	if (defined('COMPOSER_HOME') && is_dir(COMPOSER_HOME)) {
-		putenv('COMPOSER_HOME='.COMPOSER_HOME);
+		putenv('COMPOSER_HOME=' . COMPOSER_HOME);
 	}
 }
 
@@ -287,15 +310,15 @@ if (defined('USE_COMPOSER') && USE_COMPOSER === true) {
 // Compile exclude parameters
 $exclude = '';
 foreach (unserialize(EXCLUDE) as $exc) {
-	$exclude .= ' --exclude='.$exc;
+	$exclude .= ' --exclude=' . $exc;
 }
 // Deployment command
 $commands[] = sprintf(
-	'rsync -rltgoDzvO %s %s %s %s'
-	, TMP_DIR
-	, TARGET_DIR
-	, (DELETE_FILES) ? '--delete-after' : ''
-	, $exclude
+	'rsync -rltgoDzvO %s %s %s %s',
+	TMP_DIR,
+	TARGET_DIR,
+	(DELETE_FILES) ? '--delete-after' : '',
+	$exclude
 );
 
 // =======================================[ Post-Deployment steps ]===
@@ -303,8 +326,8 @@ $commands[] = sprintf(
 // Remove the TMP_DIR (depends on CLEAN_UP)
 if (CLEAN_UP) {
 	$commands['cleanup'] = sprintf(
-		'rm -rf %s'
-		, TMP_DIR
+		'rm -rf %s',
+		TMP_DIR
 	);
 }
 
@@ -316,21 +339,23 @@ foreach ($commands as $command) {
 		chdir(TMP_DIR); // Ensure that we're in the right directory
 	}
 	$tmp = array();
-	exec($command.' 2>&1', $tmp, $return_code); // Execute the command
+	exec($command . ' 2>&1', $tmp, $return_code); // Execute the command
 	// Output the result
-	printf('
+	printf(
+		'
 <span class="prompt">$</span> <span class="command">%s</span>
 <div class="output">%s</div>
-'
-		, htmlentities(trim($command))
-		, htmlentities(trim(implode("\n", $tmp)))
+',
+		htmlentities(trim($command)),
+		htmlentities(trim(implode("\n", $tmp)))
 	);
-	
+
 
 	// Error handling and cleanup
 	if ($return_code !== 0) {
 		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-		printf('
+		printf(
+			'
 <div class="error">
 Error encountered!
 Stopping the script to prevent possible data loss.
@@ -340,22 +365,23 @@ CHECK THE DATA IN YOUR TARGET DIR!
 		);
 		if (CLEAN_UP) {
 			$tmp = shell_exec($commands['cleanup']);
-			printf('
+			printf(
+				'
 
 
 Cleaning up temporary files ...
 
 <span class="prompt">$</span> <span class="command">%s</span>
 <div class="output">%s</div>
-'
-				, htmlentities(trim($commands['cleanup']))
-				, htmlentities(trim($tmp))
+',
+				htmlentities(trim($commands['cleanup'])),
+				htmlentities(trim($tmp))
 			);
 		}
 		$error = sprintf(
-			'Deployment error on %s using %s!'
-			, $_SERVER['HTTP_HOST']
-			, __FILE__
+			'Deployment error on %s using %s!',
+			$_SERVER['HTTP_HOST'],
+			__FILE__
 		);
 		error_log($error);
 		if (EMAIL_ON_ERROR) {
@@ -374,12 +400,13 @@ Cleaning up temporary files ...
 Done.
 </pre>
 </body>
+
 </html>
 <?
-	$output .= ob_get_contents();
-	ob_flush(); // Try to output everything as it happens
-	$cabeceras = 'MIME-Version: 1.0' . "\r\n";
-	$cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-	
+$output .= ob_get_contents();
+ob_flush(); // Try to output everything as it happens
+$cabeceras = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
 
 ?>
