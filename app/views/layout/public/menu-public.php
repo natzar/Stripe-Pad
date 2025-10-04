287fc81df4cb02f24f85dbce0713ef41b208724b
Beto <beto@ayesadigital.com>
2025-10-04 12:06:24 +0200

    Update homepage.php and menu-public.php

--- app/views/layout/public/menu-public.php
+++ app/views/layout/public/menu-public.php
@@ -8,8 +8,8 @@
 
     </a>
     <div class="flex items-center justify-end">
-      <a href="<?= APP_DOMAIN ?>?lang=es" class="size-5 inline-block mr-2"><img src="<?= APP_CDN ?>flags/es.png"></a>
-      <a href="<?= APP_DOMAIN ?>?lang=en" class="size-5 inline-block mr-2"><img src="<?= APP_CDN ?>flags/us.png"></a>
+      <!-- <a href="<?= APP_DOMAIN ?>?lang=es" class="size-5 inline-block mr-2"><img src="<?= APP_CDN ?>flags/es.png"></a>
+      <a href="<?= APP_DOMAIN ?>?lang=en" class="size-5 inline-block mr-2"><img src="<?= APP_CDN ?>flags/us.png"></a> -->
       <!-- <a href=" <?= APP_DOMAIN ?>signup"">
         <button class=" hidden rounded-full bg-blue-900 px-4 py-1 text-xs text-black font-semibold transition duration-200 ease-out hover:bg-blue-400 lg:block">
           <?= _('Login') ?>
@@ -45,7 +45,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
           </svg>
 
-          <?= _('Inicio') ?></a>
+          <?= _('Home') ?></a>
 
 
         <!-- <a href="<?= APP_BASE_URL ?>screencasts" class="text-base font-semibold leading-6 text-gray-400 px-3 py-1 hover:bg-gray-900 hover:text-blue-900 rounded-full">Screencasts</a> -->
@@ -104,12 +104,12 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
           </svg>
 
-          <?= _('Preguntas Frecuentes') ?></a>
+          <?= _('FAQ') ?></a>
 
         <a href="<?= APP_BASE_URL ?>pricing" class="text-base font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-900 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
           </svg>
-          <?= _('Precios') ?></a>
+          <?= _('Pricing') ?></a>
 
       </div>
       <div class="hidden lg:flex lg:flex-1 lg:justify-end items-center space-x-2">
@@ -154,16 +154,9 @@
 
 
               <a href="<?= APP_BASE_URL ?>" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Home') ?></a>
-              <a href="<?= APP_BASE_URL ?>faq" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Preguntas Frecuentes') ?></a>
-              <a href="<?= APP_BASE_URL ?>pricing" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Precios') ?></a>
-              <a href="<?= APP_BASE_URL ?>contact" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Contacto') ?></a>
-              <a href="<?= APP_BASE_URL ?>demo" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Demo') ?></a>
-              <!-- 
+              <a href="<?= APP_BASE_URL ?>faq" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('FAQ') ?></a>
+              <a href="<?= APP_BASE_URL ?>pricing" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Pricing') ?></a>
 
-
-                <a href="<?= APP_BASE_URL ?>examples" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800">Examples</a>
-
-                <a href="https://github.com/natzar/Stripe-Pad/wiki" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800">Documentation</a> -->
 
 
 
@@ -175,7 +168,7 @@
 
 
               <a href="<?= APP_DOMAIN ?>login" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Login') ?></a>
-              <a href="<?= APP_DOMAIN ?>signup" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Empezar ahora') ?></a>
+              <a href="<?= APP_DOMAIN ?>signup" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-400 hover:bg-gray-800"><?= _('Signup') ?></a>
 
 
             </div>
