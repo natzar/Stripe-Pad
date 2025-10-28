a598390fd1652063ddb6be93e8ffc7ce7d4d537f
natzar <beto.phpninja@gmail.com>
2025-10-28 14:24:39 +0100

    Update menu.php

--- landing/views/layout/menu.php
+++ landing/views/layout/menu.php
@@ -19,11 +19,11 @@
     </div>
   </div>
 
-  <header class="mx-auto w-full z-40    ">
+  <header class="mx-auto w-full z-40  bg-gray-900  ">
     <nav class="flex items-center md:justify-between w-full d:justify-start p-6 lg:px-8 " aria-label="Global">
       <div class="flex lg:flex-1">
         <!-- onclick="document.getElementById('emilio-modal').classList.remove('hidden')"  -->
-        <a href="<?= LANDING_URL ?>" class=" -m-1.5 p-1.5 text-gray-900 hover:text-blue-900 font-bold">
+        <a href="<?= LANDING_URL ?>" class=" -m-1.5 p-1.5 text-gray-100 hover:text-blue-400 font-bold">
           <span class="sr-only"><?= APP_NAME ?></span>
           <img src="<?= APP_LOGO ?>" width="40" class="inline">
           <span class=""><?= APP_NAME ?></span>
@@ -41,18 +41,18 @@
 
 
 
-        <a href="<?= LANDING_URL ?>" class="text-md font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-900 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
+        <a href="<?= LANDING_URL ?>" class="text-md font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-400 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
             <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
           </svg>
 
           <?= _('Home') ?></a>
 
 
-        <!-- <a href="<?= LANDING_URL ?>screencasts" class="text-base font-semibold leading-6 text-gray-400 px-3 py-1 hover:bg-gray-900 hover:text-blue-900 rounded-full">Screencasts</a> -->
+        <!-- <a href="<?= LANDING_URL ?>screencasts" class="text-base font-semibold leading-6 text-gray-400 px-3 py-1 hover:bg-gray-900 hover:text-blue-400 rounded-full">Screencasts</a> -->
 
         <!-- <div class="relative">
           <button type="button" onclick="$('#flyout_menu').toggleClass('hidden');" class="inline-flex items-center gap-x-1 font-semibold text-gray-900" aria-expanded="false">
-            <span class="text-md font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-900 rounded-full">Casos de uso</span>
+            <span class="text-md font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-400 rounded-full">Casos de uso</span>
             <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
             </svg>
@@ -100,13 +100,13 @@
         </div> -->
 
 
-        <a href="<?= LANDING_URL ?>faq" class="text-md font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-900 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
+        <a href="<?= LANDING_URL ?>faq" class="text-md font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-400 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
           </svg>
 
           <?= _('FAQ') ?></a>
 
-        <a href="<?= LANDING_URL ?>pricing" class="text-base font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-900 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
+        <a href="<?= LANDING_URL ?>pricing" class="text-base font-semibold leading-6 text-gray-600 px-3 py-1 hover:bg-gray-50 hover:text-blue-400 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
           </svg>
           <?= _('Pricing') ?></a>
