a8f50fd4e4874a1d6b796ff21c2645f77fcb2872
natzar <beto.phpninja@gmail.com>
2025-11-07 20:57:07 +0100

    Update menu.php

--- app/views/layout/menu.php
+++ app/views/layout/menu.php
@@ -68,7 +68,7 @@
                             <svg class="size-6 " fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                             </svg>
-                            Inicio
+                            Dashboard
                         </a>
                     </li>
 
@@ -79,11 +79,11 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                             </svg>
 
-                            Escenarios
+                            What Ever
                         </a>
                     </li>
                     <li>
-                        <div class="text-xs/6 font-semibold text-gray-400">Sidebar header</div>
+                        <div class="text-xs/6 font-semibold text-gray-400">Sidebar Subheader</div>
                     </li>
                     <li class="mb-5">
                         <a href="<?= APP_URL ?>app_conversations/unknown" class="group flex gap-x-3 rounded-md p-2 text-sm text-gray-800 hover:bg-gray-50 hover:text-blue-600">
@@ -97,48 +97,8 @@
 
 
 
-
-                    <!-- 
-
-                    <li class="mb-5">
-                        <a href="<?= APP_URL ?>app_tags" class="group flex gap-x-3 rounded-md p-2 text-sm text-gray-800 hover:bg-gray-50 hover:text-blue-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
-                            </svg>
-                            Etiquetas
-                        </a>
-                    </li> -->
-
-
-
-                    <!-- <li class="mb-5">
-                        <a href="<?= APP_URL ?>app_calendar" class="group flex gap-x-3 rounded-md p-2 text-sm text-gray-800 hover:bg-gray-50 hover:text-blue-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                            </svg>
-                            Calendario
-                        </a>
-                    </li>
-                    <li class="mb-5">
-                        <a href="<?= APP_URL ?>app_documents" class="group flex gap-x-3 rounded-md p-2 text-sm text-gray-800 hover:bg-gray-50 hover:text-blue-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
-                            </svg>
-
-                            Documentos
-                        </a>
-                    </li> -->
-
-                    <!-- <li class="mb-5">
-                        <a href="<?= APP_URL ?>app_faq" class="group flex gap-x-3 rounded-md p-2 text-sm text-gray-800 hover:bg-gray-50 hover:text-blue-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
-                            </svg> Preguntas y Respuestas
-                        </a>
-                    </li> -->
                     <li>
-                        <div class="text-xs/6 font-semibold text-gray-400">Configuración</div>
+                        <div class="text-xs/6 font-semibold text-gray-400">User</div>
                     </li>
 
 
@@ -150,7 +110,7 @@
                             </svg>
 
 
-                            Preferencias Usuario
+                            <?= _('User Profile') ?>
                         </a>
                     </li>
                     <li>
@@ -161,7 +121,7 @@
                             </svg>
 
 
-                            Ayuda
+                            <?= _('Help & Support') ?>
                         </a>
                     </li>
 
@@ -331,9 +291,9 @@
 
                         <div id="user-submenu" class="hidden  absolute right-0 z-10 mt-2.5 w-64 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-50 outline-none", Not Active: "" -->
-                            <a href="<?= APP_URL ?>profile" class="block px-3 py-1 text-sm text-gray-900 hover:font-bold hover:text-black" role="menuitem" tabindex="-1" id="user-menu-item-0"><?= _('Preferencias') ?></a>
+                            <a href="<?= APP_URL ?>profile" class="block px-3 py-1 text-sm text-gray-900 hover:font-bold hover:text-black" role="menuitem" tabindex="-1" id="user-menu-item-0"><?= _('User Profile') ?></a>
                             <a href="<?= APP_URL ?>actionLogout" class="block px-3 py-1 text-sm text-gray-900 hover:font-bold hover:text-black" role="menuitem" tabindex="-1" id="user-menu-item-1">
-                                <?= _('Cerrar Sesión') ?></a>
+                                <?= _('Log Out') ?></a>
                         </div>
                     </div>
                 </div>
