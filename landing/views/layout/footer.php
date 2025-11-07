0df7a20aecd8d6e48f71f4957d20061a01d523ff
natzar <beto.phpninja@gmail.com>
2025-11-07 20:52:46 +0100

    Update footer.php

--- landing/views/layout/footer.php
+++ landing/views/layout/footer.php
@@ -186,9 +186,9 @@
       </form>
     </div>
     <div class="mt-10 border-t border-white/10 pt-8 ">
-      <p class="text-xs leading-5 text-gray-400">&copy; <a href="<?= LANDING_URL ?>"><?= LANDING_URL ?></a> <?= $SEO_TITLE ?> &copy; <?= Date("Y") ?> Ayesa Digital SLU. All rights reserved</p>
+      <p class="text-xs leading-5 text-gray-400">&copy; <a href="<?= LANDING_URL ?>"><?= LANDING_URL ?></a> <?= $SEO_TITLE ?> &copy; <?= Date("Y") ?> All rights reserved</p>
       <p class="text-xs leading-5 text-gray-500">
-        Powered by <a href="//stripepad.com">Stripe Pad v.0.0.1 </a> · Get Support: <a href="mailto:<?= ADMIN_EMAIL ?>"><?= ADMIN_EMAIL ?></a></p>
+        Powered by <a href="//stripepad.com">Stripe Pad v.<?= STRIPE_PAD_VERSION ?></a> · Get Support: <a href="mailto:<?= ADMIN_EMAIL ?>"><?= ADMIN_EMAIL ?></a></p>
 
 
     </div>
